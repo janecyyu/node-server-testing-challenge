da3cb81352f1582b1d923325cf51159e318b4612
janecyyu <janecyyu@gmail.com>
2020-05-21 13:56:14 -0700

    add test

---
+++ api/sever.test.js
@@ -0,0 +1,39 @@
+const supertest = require("supertest");
+const server = require("./server");
+
+describe("server", () => {
+  it("can run", () => {
+    expect(true).toBeTruthy();
+  });
+  describe("GET /", () => {
+    it("should return http status 200 ok", () => {
+      return (
+        supertest(server)
+          .get("/")
+          //.expect(200) from supertest
+          .then((response) => {
+            //from jest
+            expect(response.status).toBe(200);
+          })
+      );
+    });
+    it("should return {api:up}", () => {
+      return supertest(server)
+        .get("/")
+        .then((res) => {
+          expect(res.body).toEqual({ api: "up" });
+          expect(res.body.api).toBe("up");
+          expect(res.body.api).toBeDefined();
+        });
+    });
+  });
+  describe("GET /users", () => {
+    it("should return an array", () => {
+      return supertest(server)
+        .get("/users")
+        .then((res) => {
+          expect(Array.isArray(res.body)).toBe(true);
+        });
+    });
+  });
+});
